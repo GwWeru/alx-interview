b53046db82b1546fcfc2b3d290c08a29970c2c5d
weru <georgeweu143@gmail.com>
2023-07-03 18:03:01 +0300

    pascal-triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+def pascal_triangle(n):
+    triangle = []
+    for i in range(n):
+        row = [1]*(i + 1)
+        if i > 1:
+            _row = triangle[i - 1]
+            for a in range(i - 1):
+                row[a] = _row[a - 1]+_row[a]
+        triangle.append(row)
+    return triangle
+n = int(input('Enter a number. '))
+x = pascal_triangle(n)
+print(x)
+for y in (x):
+    print(y)
